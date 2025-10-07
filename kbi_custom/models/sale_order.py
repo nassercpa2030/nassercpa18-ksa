b3d31ffbe73ffc2c827707556b67c391c5cd760a
NK_IT <m.mohamed@nassercpa.com>
2025-10-07 14:46:02 +0300

    Update sale_order.py

--- kbi_custom/models/sale_order.py
+++ kbi_custom/models/sale_order.py
@@ -188,7 +188,7 @@ class SaleOrder ( models.Model ) :
     def action_view_invoice(self , invoices=False) :
         if not invoices :
             #invoices = self.mapped ( 'invoice_ids' )
-            invoices = self.env['account.move'].search ( [('sale_order_id' , '=' , self.name)] )
+            invoices = self.env['account.move'].search ( [('sale_order_test' , '=' , self.name)] )
         action = self.env['ir.actions.actions']._for_xml_id ( 'account.action_move_out_invoice_type' )
         if len ( invoices ) > 1 :
             action['domain'] = [('id' , 'in' , invoices.ids)]
