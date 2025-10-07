77a7048ce244e3f53e2056763546d4b304d57cdf
NK_IT <m.mohamed@nassercpa.com>
2025-10-07 14:32:44 +0300

    Update sale_order.py

--- kbi_custom/models/sale_order.py
+++ kbi_custom/models/sale_order.py
@@ -185,6 +185,39 @@ class SaleOrder ( models.Model ) :
                 }
             }
 
+    def action_view_invoice(self , invoices=False) :
+        if not invoices :
+            #invoices = self.mapped ( 'invoice_ids' )
+            invoices = self.env['account.move'].search ( [('invoice_origin' , '=' , self.name)] )
+        action = self.env['ir.actions.actions']._for_xml_id ( 'account.action_move_out_invoice_type' )
+        if len ( invoices ) > 1 :
+            action['domain'] = [('id' , 'in' , invoices.ids)]
+        elif len ( invoices ) == 1 :
+            form_view = [(self.env.ref ( 'account.view_move_form' ).id , 'form')]
+            if 'views' in action :
+                action['views'] = form_view + [(state , view) for state , view in action['views'] if view != 'form']
+            else :
+                action['views'] = form_view
+            action['res_id'] = invoices.id
+        else :
+            action = {'type' : 'ir.actions.act_window_close'}
+
+        context = {
+            'default_move_type' : 'out_invoice' ,
+        }
+        if len ( self ) == 1 :
+            context.update ( {
+                'default_partner_id' : self.partner_id.id ,
+                'default_partner_shipping_id' : self.partner_shipping_id.id ,
+                'default_invoice_payment_term_id' : self.payment_term_id.id or self.partner_id.property_payment_term_id.id or
+                                                    self.env['account.move'].default_get (
+                                                        ['invoice_payment_term_id'] ).get (
+                                                        'invoice_payment_term_id' ) ,
+                'default_invoice_origin' : self.name ,
+            } )
+        action['context'] = context
+        return action
+
     @api.onchange ( 'number_700_sale' , 'cr_number_sale' )
     def _onchange_customer_by_number(self) :
         """تحديث العميل تلقائيًا بناءً على الرقم المدخل"""
