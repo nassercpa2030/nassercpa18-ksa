391b0acba2f0e016b9819f9b4c9028303c19cbfb
NK_IT <m.mohamed@nassercpa.com>
2025-10-08 10:14:35 +0300

    Update account.py

--- kbi_custom/models/account.py
+++ kbi_custom/models/account.py
@@ -17,49 +17,61 @@ class AccountMove ( models.Model ) :
     )
     sale_order_test = fields.Char ( string="Sale Order Refrence" , readonly=False , Required=False )
     sale_order_id = fields.Many2one ( 'sale.order' , string='Sale Order' , domain="[('partner_id','=',partner_id)]" )
+    invoice_count_odoo16 = fields.Integer(string="Same Invoices Count", compute="_compute_invoices", store=True)
 
-    def action_post(self) :
-        # ترحيل الفواتير فورًا مع تجاوز جميع تحقق E-Invoicing
-        self.with_context ( disable_sa_edi_checks=True )._post ( soft=False )
-        # لو هناك أي wizard تلقائي للفواتير، نعرضه
-        if autopost_bills_wizard := self._show_autopost_bills_wizard () :
-            return autopost_bills_wizard
-        return True
+@api.depends('sale_order_test')
+def _compute_invoices(self) :
+        for invoice in self :
+            extra_invoices = self.env['account.move'].search_count( [
+                ('sale_order_test' , 'ilike' , invoice.sale_order_test) ,
+                ('move_type' , '=' , 'out_invoice')
+            ] )
+            invoice.invoice_count_odoo16 = extra_invoices
 
-    @api.onchange ( 'sale_order_id' )
-    def _onchange_sale_order_id(self) :
-        """تحديث الحقول تلقائيًا عند اختيار Sale Order"""
-        for rec in self :
-            if rec.sale_order_id :
-                # ضبط البرتنر تلقائيًا
-                rec.partner_id = rec.sale_order_id.partner_id.id
-                # ضبط عنوان الشحن
-                rec.partner_shipping_id = rec.sale_order_id.partner_shipping_id.id
-                # ضبط شروط الدفع
-                rec.invoice_payment_term_id = (
-                        rec.sale_order_id.payment_term_id
-                        or rec.partner_id.property_payment_term_id
-                        or self.env['account.move'].default_get ( ['invoice_payment_term_id'] ).get (
-                    'invoice_payment_term_id' )
-                )
-                # ضبط المرجع / المصدر
-                rec.invoice_origin = rec.sale_order_id.name
+def action_post(self) :
+    # ترحيل الفواتير فورًا مع تجاوز جميع تحقق E-Invoicing
+    self.with_context ( disable_sa_edi_checks=True )._post ( soft=False )
+    # لو هناك أي wizard تلقائي للفواتير، نعرضه
+    if autopost_bills_wizard := self._show_autopost_bills_wizard () :
+        return autopost_bills_wizard
+    return True
 
-    def _compute_analytic_distribution(self) :
-        for rec in self :
-            analytic_name = ''
-            for line in rec.line_ids :
-                if hasattr ( line , 'analytic_distribution' ) and line.analytic_distribution :
-                    analytic_ids = list ( line.analytic_distribution.keys () )
-                    try :
-                        analytic_id = int ( analytic_ids[0] )
-                        analytic = self.env['account.analytic.account'].browse ( analytic_id )
-                        if analytic.exists () :
-                            analytic_name = analytic.name
-                            break
-                    except (ValueError , TypeError) :
-                        continue
-            rec.analytic_acc_desc = analytic_name
+
+@api.onchange ( 'sale_order_id' )
+def _onchange_sale_order_id(self) :
+    """تحديث الحقول تلقائيًا عند اختيار Sale Order"""
+    for rec in self :
+        if rec.sale_order_id :
+            # ضبط البرتنر تلقائيًا
+            rec.partner_id = rec.sale_order_id.partner_id.id
+            # ضبط عنوان الشحن
+            rec.partner_shipping_id = rec.sale_order_id.partner_shipping_id.id
+            # ضبط شروط الدفع
+            rec.invoice_payment_term_id = (
+                    rec.sale_order_id.payment_term_id
+                    or rec.partner_id.property_payment_term_id
+                    or self.env['account.move'].default_get ( ['invoice_payment_term_id'] ).get (
+                'invoice_payment_term_id' )
+            )
+            # ضبط المرجع / المصدر
+            rec.invoice_origin = rec.sale_order_id.name
+
+
+def _compute_analytic_distribution(self) :
+    for rec in self :
+        analytic_name = ''
+        for line in rec.line_ids :
+            if hasattr ( line , 'analytic_distribution' ) and line.analytic_distribution :
+                analytic_ids = list ( line.analytic_distribution.keys () )
+                try :
+                    analytic_id = int ( analytic_ids[0] )
+                    analytic = self.env['account.analytic.account'].browse ( analytic_id )
+                    if analytic.exists () :
+                        analytic_name = analytic.name
+                        break
+                except (ValueError , TypeError) :
+                    continue
+        rec.analytic_acc_desc = analytic_name
 
 
 class AccountMoveLine ( models.Model ) :
