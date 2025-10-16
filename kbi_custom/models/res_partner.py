7be5d27f511e4cf01fa20418d2f6d424a7f38720
NK_IT <m.mohamed@nassercpa.com>
2025-10-16 15:39:50 +0300

    Update res_partner.py

--- kbi_custom/models/res_partner.py
+++ kbi_custom/models/res_partner.py
@@ -20,7 +20,7 @@ class ResPartner ( models.Model ) :
     city_id = fields.Many2one ( comodel_name='res.country.state.city' , string='City' )
     agreement_id = fields.Many2one ( 'kbi.sale.agreement' , string='Agreements' )
     nationality = fields.Char ( "Nationality" )
-    manager_team= fields.Many2one( comodel_name="crm.team" , string='Manager Team', related="sale_order_ids.team_id", store=True,readonly=True)
+    manager_team= fields.Many2one( comodel_name="crm.team" , string='Manager Team', related="sale_order_ids.team_id", store=True)
     is_broker = fields.Boolean ( string='Broker' )
     name_english = fields.Char ( String="English name" , readonly=False , store=True )
     partner_vat_placeholder = fields.Char ( string="Vat Number" , readonly=False )
