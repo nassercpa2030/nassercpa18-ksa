0eb828683fecd36047097db9b2025084d859d123
NK_IT <m.mohamed@nassercpa.com>
2025-10-16 15:54:16 +0300

    Update res_partner.py

--- kbi_custom/models/res_partner.py
+++ kbi_custom/models/res_partner.py
@@ -20,20 +20,36 @@ class ResPartner ( models.Model ) :
     city_id = fields.Many2one ( comodel_name='res.country.state.city' , string='City' )
     agreement_id = fields.Many2one ( 'kbi.sale.agreement' , string='Agreements' )
     nationality = fields.Char ( "Nationality" )
-    manager_team= fields.Many2one( comodel_name="crm.team" , string='Manager Team', related="sale_order_ids.team_id", store=True)
+    manager_team = fields.Many2one ( comodel_name="crm.team" , string='Manager Team' , compute="_compute_manager_team" ,
+                                     readonly=False , store=True )
     is_broker = fields.Boolean ( string='Broker' )
     name_english = fields.Char ( String="English name" , readonly=False , store=True )
     partner_vat_placeholder = fields.Char ( string="Vat Number" , readonly=False )
     number_700 = fields.Char ( string="700 Number" , readonly=False )
-    cr_number_sale=fields.Char(related="sale_order_ids.cr_number_sale",string="Commercial number" ,readonly=False,store=True)
-    property_account_payable_id = fields.Many2one(commodel_name="account.account", domain=[('code', '=', '21011001')],store=True,readonly=False,string='Account Payable', default=lambda self: self.env['account.account'].search([('code', '=', '21011001')], limit=1).id)
-    fax_number=fields.Char(string='FAX',readonly=False,required=False)
+    cr_number_sale = fields.Char ( related="sale_order_ids.cr_number_sale" , string="Commercial number" ,
+                                   readonly=False , store=True )
+    property_account_payable_id = fields.Many2one ( commodel_name="account.account" ,
+                                                    domain=[('code' , '=' , '21011001')] , store=True , readonly=False ,
+                                                    string='Account Payable' ,
+                                                    default=lambda self : self.env['account.account'].search (
+                                                        [('code' , '=' , '21011001')] , limit=1 ).id )
+    fax_number = fields.Char ( string='FAX' , readonly=False , required=False )
+
     @api.constrains ( 'number_700' )
     def _check700_number(self) :
         pattern = r'^7\d*$'
         for rec in self :
             if rec.number_700 and not re.match ( pattern , rec.number_700 ) :
                 raise ValidationError ( "You must enter numbers only and start with 7" )
+
+    @api.depends ( 'sale_order_ids.team_id' )
+    def _compute_manager_team(self) :
+        for partner in self :
+            if partner.sale_order_count > 0 :
+                # هيرجع الفريق من أول order (تقدر تعدلها لو عايز آخر order مثلاً)
+                partner.manager_team = partner.sale_order_ids[0].team_id.id
+            else :
+                partner.manager_team = False
 
     def action_merge_specific_duplicates(self) :
         """
