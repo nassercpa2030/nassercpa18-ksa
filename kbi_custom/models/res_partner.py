8fff8491f8d36b29b9f0a610bab37e13c99155d0
NK_IT <m.mohamed@nassercpa.com>
2025-10-16 18:16:35 +0300

    Update res_partner.py

--- kbi_custom/models/res_partner.py
+++ kbi_custom/models/res_partner.py
@@ -20,7 +20,7 @@ class ResPartner ( models.Model ) :
     city_id = fields.Many2one ( comodel_name='res.country.state.city' , string='City' )
     agreement_id = fields.Many2one ( 'kbi.sale.agreement' , string='Agreements' )
     nationality = fields.Char ( "Nationality" )
-    manager_team = fields.Many2one ( comodel_name="res.users" , string='Manager',related="sale_order_ids.user_id" , store=True,readonly=False )
+    manager_team = fields.Many2one ( comodel_name="res.users" , string='Manager',related="x_studio_related_field_7pm_1j7mp6p7k" , store=True,readonly=False )
     is_broker = fields.Boolean ( string='Broker' )
     name_english = fields.Char ( String="English name" , readonly=False , store=True )
     partner_vat_placeholder = fields.Char ( string="Vat Number" , readonly=False )
@@ -40,21 +40,7 @@ class ResPartner ( models.Model ) :
         for rec in self :
             if rec.number_700 and not re.match ( pattern , rec.number_700 ) :
                 raise ValidationError ( "You must enter numbers only and start with 7" )
-
- 
-    @api.depends('sale_order_ids.user_id', 'sale_order_ids.date_order', 'sale_order_ids.state')
-    def _compute_manager_team(self):
-        for partner in self:
-            # فلترة أوامر البيع المكتملة فقط
-            sale_orders = partner.sale_order_ids.filtered(lambda o: o.state in ['sale','done'])
-            if sale_orders:
-                # ترتيب حسب التاريخ الأخير
-                last_order = sale_orders.sorted(key=lambda o: o.date_order or fields.Datetime.now(), reverse=True)[0]
-                partner.manager_team = last_order.user_id
-            else:
-                partner.manager_team = False
-
-    
+   
     def action_merge_specific_duplicates(self) :
         """
         دمج كل الشركاء المكررة بناءً على الاسم فقط.
