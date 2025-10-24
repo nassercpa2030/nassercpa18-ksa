9de3d743eb53ddb55f26da9467fa62f41ca7416d
NK_IT <m.mohamed@nassercpa.com>
2025-10-24 06:28:40 +0300

    Update models.py

--- bi_project_custom/models/models.py
+++ bi_project_custom/models/models.py
@@ -185,6 +185,10 @@ class SaleOrder ( models.Model ) :
                 'sticky' : False ,  # لو True الرسالة تظل حتى يغلقها المستخدم
             }
         }
+    @api.multi   
+    def _action_print_mutalba_report(self):
+        report = self.env['ir.actions.report'].browse(1079)
+        return report.report_action(self)
 
     def _auto_code(self) :
         sequence_map = {
