e0be47d6bad8ec0bb2c1838efeff60b830de6425
NK_IT <m.mohamed@nassercpa.com>
2025-10-28 11:42:42 +0300

    Update sale_order.py

--- kbi_custom/models/sale_order.py
+++ kbi_custom/models/sale_order.py
@@ -187,8 +187,8 @@ class SaleOrder ( models.Model ) :
     @api.depends("product_public_name","account_year")
     def get_project_name(self):
          for rec in self:
-            if  rec.project_name == "" : 
-                if rec.auto_contract_name and
+            if  not rec.project_name  : 
+                if rec.auto_contract_name :
                    if  rec.product_public_name and rec.account_year  :
                        rec.project_name=f"{rec.product_public_name} {rec.account_year}"
                    elif rec.product_public_name :
@@ -197,6 +197,7 @@ class SaleOrder ( models.Model ) :
                        rec.project_name ="لم يتم تحديد الخدمة لهذا العقد "
                    else :
                        rec.project_name ="لم يتم تحديد الخدمة والسنة لهذا العقد "
+               
                  
      # @api.depends("x_studio_contract_service")           
       #def get_audit_date (self):
