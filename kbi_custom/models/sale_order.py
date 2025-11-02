a0b9d5502c87afefbb8e5dda5db2fb2dd1dfae2d
NK_IT <m.mohamed@nassercpa.com>
2025-11-02 10:57:21 +0300

    Update sale_order.py

--- kbi_custom/models/sale_order.py
+++ kbi_custom/models/sale_order.py
@@ -581,7 +581,8 @@ class SaleOrder ( models.Model ) :
             'type' : 'ir.actions.act_window' ,
             'view_mode' : 'list,form' ,
             'res_model' : 'account.payment' ,
-            'domain' : ['|',('sale_order_id' , 'in' , self.ids or []),('id', '=', 0)] ,
+            #'domain' : [('sale_order_id' , 'in' , self.ids)] ,
+             'domain': ['|', ('sale_order_ids', 'in', [self.id]), ('id', '=', 0)],
             'context' : {
                 'default_sale_order_ids' : self.id ,
                 'default_partner_id' : self.partner_id.id ,
