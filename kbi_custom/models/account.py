671deab0cf6be528416a76242d0186a03d23501b
NK_IT <m.mohamed@nassercpa.com>
2025-12-17 13:32:59 +0300

    Update account.py

--- kbi_custom/models/account.py
+++ kbi_custom/models/account.py
@@ -118,7 +118,7 @@ class AccountPayment(models.Model):
             if rec.journal_id:
                 if rec.journal_id.id == 153:
                     #rec.destination_account_id = self.env['account.account'].browse(1142)
-                    rec.destination_account_id = 1142
+                    #rec.destination_account_id = 1142
                     rec.partner_id = 80000
             else:
                 rec.destination_account_id = False
