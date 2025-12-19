112c296fb31986a704e4134f31235539cd328b53
NK_IT <m.mohamed@nassercpa.com>
2025-12-19 12:28:21 +0300

    Update sale_order.py

--- kbi_custom/models/sale_order.py
+++ kbi_custom/models/sale_order.py
@@ -724,7 +724,7 @@ class SaleOrder ( models.Model ) :
     year3=fields.Char(string="Year_3",readonly=False,deFault=False)
     uuid = fields.Char ( string='UUID' )
     final_close_entry_date = fields.Date ( string="تاريخ قيد الايراد" , compute='_compute_final_close_entry_date',readonly=False,index=True,searchable=True)
-    close_entry_date = fields.Date (string="Close Entry Date" ,store=True, readonly=False ,searchable=True)
+    close_entry_date = fields.Date (string="Close Entry Date" ,store=True, readonly=False ,index=True,searchable=True)
     close_entry_year = fields.Integer ( string="Close Entry Year" ,store=True, readonly=False,searchable=True )
     validity_date = fields.Date ( string='Validity Date' ,
                                   default=fields.Date.today () + datetime.timedelta ( days=30 ) )
@@ -745,11 +745,7 @@ class SaleOrder ( models.Model ) :
     @api.depends('final_close_entry_date')
     def _compute_final_close_entry_date(self):
         for rec in self:
-            # هنا تحسب final_close_entry_date
-            rec.final_close_entry_date = fields.Date.today()  # مثال لحسابها
-            # لو close_entry_date فاضي، انسخه
-            if not rec.close_entry_date:
-                rec.close_entry_date = rec.final_close_entry_date
+            rec.close_entry_date = rec.final_close_entry_date
             
     
     @api.depends('audit_date')
