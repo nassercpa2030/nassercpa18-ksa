1ab4d215d4cdc17cbdc8e3fc5bb99f0ab25f6247
NK_IT <m.mohamed@nassercpa.com>
2025-12-24 13:11:24 +0300

    Update models.py

--- bi_project_custom/models/models.py
+++ bi_project_custom/models/models.py
@@ -287,11 +287,11 @@ class SaleOrder ( models.Model ) :
                         'res_id' : order.id
                     } )
 
-            for invoice in order.invoice_ids :
-                existing_attachment_ids = invoice.attachment_ids.ids  # المرفقات الحالية للفاتورة
-                for attachment in order.invoice_attachements_ids:
-                    if attachment.id not in existing_attachment_ids:
-                        attachment.copy({'res_model': 'account.move', 'res_id': invoice.id})
+            #for invoice in order.invoice_ids :
+               # existing_attachment_ids = invoice.attachment_ids.ids  # المرفقات الحالية للفاتورة
+                #for attachment in order.invoice_attachements_ids:
+                  #  if attachment.id not in existing_attachment_ids:
+                       # attachment.copy({'res_model': 'account.move', 'res_id': invoice.id})
             # order.sudo().invoice_attachements_ids = [(6, 0, attachments.ids)]
 
     @api.model
