bf5303631490cacba68256959e08b9a4160dcb33
NK_IT <m.mohamed@nassercpa.com>
2025-12-24 12:18:54 +0300

    Update models.py

--- bi_project_custom/models/models.py
+++ bi_project_custom/models/models.py
@@ -1,5 +1,8 @@
 from odoo import models , fields , api , _
 from odoo.exceptions import ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountMove ( models.Model ) :
