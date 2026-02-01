9dc080b41901113dfe8ae2c1b7295689c7e61187
NK_IT <m.mohamed@nassercpa.com>
2026-02-01 17:23:15 +0300

    Update res_config_settings.py

--- odashboard-18.0.1.0.0/odashboard/models/res_config_settings.py
+++ odashboard-18.0.1.0.0/odashboard/models/res_config_settings.py
@@ -1,77 +1,32 @@
 # -*- coding: utf-8 -*-
 import uuid
 import logging
+from datetime import datetime, timedelta
 from odoo import models, fields, api, _
 from ..hooks import post_init_hook
 
 _logger = logging.getLogger(__name__)
 
-
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    odashboard_plan = fields.Char(
-        string='Odashboard Plan',
-        config_parameter="odashboard.plan"
-    )
-    odashboard_key = fields.Char(
-        string="Odashboard Key",
-        config_parameter="odashboard.key"
-    )
-    odashboard_key_synchronized = fields.Boolean(
-        string="Key Synchronized",
-        config_parameter="odashboard.key_synchronized",
-        readonly=True
-    )
-    odashboard_uuid = fields.Char(
-        string="Odashboard UUID",
-        config_parameter="odashboard.uuid",
-        readonly=True
-    )
-    odashboard_is_free_trial = fields.Boolean(
-        string="Is Free Trial",
-        config_parameter="odashboard.is_free_trial",
-        readonly=True
-    )
-    odashboard_free_trial_end_date = fields.Char(
-        string="Free Trial End Date",
-        config_parameter="odashboard.free_trial_end_date",
-        readonly=True
-    )
-
-    @api.model
-    def get_values(self):
-        """Load real license values from parameters"""
-        res = super(ResConfigSettings, self).get_values()
-        config = self.env['ir.config_parameter'].sudo()
-
-        res.update({
-            'odashboard_uuid': config.get_param('odashboard.uuid', ''),
-            'odashboard_key': config.get_param('odashboard.key', ''),
-            'odashboard_plan': config.get_param('odashboard.plan', ''),
-            'odashboard_key_synchronized': config.get_param('odashboard.key_synchronized', 'False') == 'True',
-            'odashboard_is_free_trial': config.get_param('odashboard.is_free_trial', 'False') == 'True',
-            'odashboard_free_trial_end_date': config.get_param('odashboard.free_trial_end_date', ''),
-        })
-        return res
+    odashboard_plan = fields.Char(string='Odashboard Plan', config_parameter="odashboard.plan")
+    odashboard_key = fields.Char(string="Odashboard Key", config_parameter="odashboard.key")
+    odashboard_key_synchronized = fields.Boolean(string="Key Synchronized",
+                                                 config_parameter="odashboard.key_synchronized", readonly=True)
+    odashboard_uuid = fields.Char(string="Odashboard UUID", config_parameter="odashboard.uuid", readonly=True)
+    odashboard_is_free_trial = fields.Boolean(string="Is Free Trial",
+                                              config_parameter="odashboard.is_free_trial", readonly=True)
+    odashboard_free_trial_end_date = fields.Char(string="License Valid Until",
+                                                config_parameter="odashboard.free_trial_end_date", readonly=True)
 
     def get_my_key(self):
-        """Call post_init_hook to create internal 10-year license"""
+        """Generate internal 10-year license and refresh view"""
         post_init_hook(self.env)
-        # لا نحتاج refresh بعد التعديل لأنه يتم قراءة القيم من get_values
         return {
             'type': 'ir.actions.client',
             'tag': 'reload',
         }
-
-    def _clear_odashboard_data(self):
-        """Clear all odashboard-related configuration data"""
-        config_params = self.env['ir.config_parameter'].sudo()
-        config_params.set_param('odashboard.key_synchronized', False)
-        config_params.set_param('odashboard.key', '')
-        config_params.set_param('odashboard.plan', '')
-        config_params.set_param('odashboard.is_free_trial', False)
-        config_params.set_param('odashboard.free_trial_end_date', '')
 
     def synchronize_key(self):
         """Local synchronize: just mark as synchronized"""
@@ -83,7 +38,12 @@ class ResConfigSettings(models.TransientModel):
 
     def desynchronize_key(self):
         """Desynchronize key locally"""
-        self._clear_odashboard_data()
+        config_params = self.env['ir.config_parameter'].sudo()
+        config_params.set_param('odashboard.key_synchronized', False)
+        config_params.set_param('odashboard.key', '')
+        config_params.set_param('odashboard.plan', '')
+        config_params.set_param('odashboard.is_free_trial', False)
+        config_params.set_param('odashboard.free_trial_end_date', '')
         return {
             'type': 'ir.actions.client',
             'tag': 'reload',
