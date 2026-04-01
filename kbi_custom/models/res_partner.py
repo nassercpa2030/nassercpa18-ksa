4e1a873e927ef45eab50bd9710ec3e3f4a2aa042
NK_IT <m.mohamed@nassercpa.com>
2026-04-01 14:13:15 +0300

    Update res_partner.py

--- kbi_custom/models/res_partner.py
+++ kbi_custom/models/res_partner.py
@@ -428,7 +428,7 @@ class Recruiter ( models.Model ) :
                                            placeholder="Enter Related Contact" )
     request_employee_manager = fields.Many2one ( 'res.users' , string='المدير ' , required=True )
     contract_state = fields.Selection ( related='contract_id.state' , string='حالة العقد' , store=True )
-    residency_visa_number = fields.Char (string="رقم الإقامة" ,store=True)
+    residency_visa_number = fields.Integer (string="رقم الهوية /رقم الإقامة" ,store=True)
     border_number = fields.Integer ( string="رقم  الحدود" , store=True )
     iqama_expiry_date = fields.Date ( string="تاريخ انتهاء الإقامة" , store=True )
     start_working_date = fields.Date ( string="تاريخ المباشرة" , compute="_compute_start_working_date" )
