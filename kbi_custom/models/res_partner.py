ed6f76b0744375276b1134aa4424a3f92ff7c641
NK_IT <m.mohamed@nassercpa.com>
2026-04-20 15:35:15 +0300

    Update res_partner.py

--- kbi_custom/models/res_partner.py
+++ kbi_custom/models/res_partner.py
@@ -325,7 +325,7 @@ class ResPartner ( models.Model ) :
                                             placeholder="Enter Analytic Account for employee" )
 
     key_information = fields.Boolean(string="المعلومات الرئيسية",default=False,help="عند تفعيل هذا الحقل يتم عرض المعلومات الرئيسية الخاصة بجهة الاتصال الحالية.")
-    additional_information = fields.Boolean(string="المعلومات الفرعية",default=False,help="عند تفعيل هذا الحقل يتم عرض المعلومات الفرعية او الغير أساسسية الخاصة بجهة الاتصال الحالية.")
+    additional_information = fields.Boolean(string="المعلومات الفرعية",default=True,help="عند تفعيل هذا الحقل يتم عرض المعلومات الفرعية او الغير أساسسية الخاصة بجهة الاتصال الحالية.")
     nationality = fields.Char ( "Nationality" )
     email = fields.Char ( "Email" , required=True , store=True )
     real_company_name = fields.Char ( string="أسم الشركة لتقرير التسعير" , readonly=False , store=True )
