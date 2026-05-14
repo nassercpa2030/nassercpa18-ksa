7087fe81df59741a0f14349650f6e6178a86a677
NK_IT <m.mohamed@nassercpa.com>
2026-05-14 15:14:29 +0300

    Update call_mobile.js

--- kbi_custom/static/JS/call_mobile.js
+++ kbi_custom/static/JS/call_mobile.js
@@ -1,3 +1,57 @@
+// record data from browser
+odoo.define('your_module.voice_recorder', function (require) {
+    "use strict";
+
+    const rpc = require('web.rpc');
+
+    let chunks = [];
+
+    async function startRecording(leadId) {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true
+        });
+
+        const recorder = new MediaRecorder(stream);
+
+        recorder.ondataavailable = e => {
+            chunks.push(e.data);
+        };
+
+        recorder.onstop = async () => {
+
+            const blob = new Blob(chunks, {
+                type: 'audio/webm'
+            });
+
+            const reader = new FileReader();
+
+            reader.readAsDataURL(blob);
+
+            reader.onloadend = async () => {
+
+                const base64data = reader.result.split(',')[1];
+
+                await rpc.query({
+                    model: 'crm.lead',
+                    method: 'action_transcribe_audio',
+                    args: [[leadId], base64data],
+                });
+
+                location.reload();
+            };
+        };
+
+        recorder.start();
+
+        setTimeout(() => {
+            recorder.stop();
+        }, 10000);
+    }
+
+    window.startCRMRecording = startRecording;
+});
+
 // my_crm_call/static/src/js/call_mobile.js
 odoo.define('my_crm.call_mobile_js', function (require) {
     "use strict";
