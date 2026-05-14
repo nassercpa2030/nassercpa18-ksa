41d1a16889671d1cc029fbf4f901d39bf5896161
NK_IT <m.mohamed@nassercpa.com>
2026-05-14 16:38:12 +0300

    Update __manifest__.py

--- kbi_custom/__manifest__.py
+++ kbi_custom/__manifest__.py
@@ -1,21 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': "KBI CRM Customization",
-
-    'summary': """
-        CRM Customization
-    """,
-
-    'description': """
-        CRM Customization
-    """,
-
+    'summary': "CRM Customization",
+    'description': "CRM Customization",
     'author': "KnowledgeBI",
-
     'website': "https://www.yourcompany.com",
-
     'category': 'Crm',
-
     'version': '0.1',
 
     'depends': [
@@ -31,48 +21,36 @@
     ],
 
     'data': [
-
         'security/security.xml',
         'security/ir.model.access.csv',
-
         'data/sequence.xml',
-
         'views/analytic.xml',
         'views/analytic_profit_loss_views.xml',
-
         'report/analytic_profit_loss_report.xml',
-
         'views/sale_order.xml',
         'views/product.xml',
-
         'views/crm_lead.xml',
-
         'views/res_partner.xml',
         'views/agreement.xml',
         'views/payment.xml',
         'views/res_config_settings.xml',
         'views/order_verify.xml',
-
         'views/report_vendor_batch_bill.xml',
         'views/motalba_all.xml',
-
         'views/paperformat.xml',
-
         'views/external_layout_seti.xml',
         'views/external_layout_batch_bill.xml',
-
         'views/report_customer_invoice_report2.xml',
         'views/report_customer_project_report.xml',
-
         'views/hr_payslip_line_style.xml',
+    ],
 
-    ],
-'assets': {
-    'web.assets_backend': [
-        'kbi_custom/static/src/js/voice_to_text.js',
-    ],
-},
+    'assets': {
+        'web.assets_backend': [
+            'kbi_custom/static/src/js/voice_to_text.js',
+        ],
+    },
+
     'installable': True,
-
     'application': False,
 }
