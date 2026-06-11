eb4d2c17cb90e65ce39ec5886a8dc71e2aa21004
NK_IT <m.mohamed@nassercpa.com>
2026-06-11 21:48:01 +0300

    Update sale_order.py

--- kbi_custom/models/sale_order.py
+++ kbi_custom/models/sale_order.py
@@ -136,7 +136,7 @@ class SaleOrder ( models.Model ) :
                                             compute='_compute_analytic_account_id' , readonly=False , store=True )
     analytic_account_id_assigned = fields.Many2one ( 'account.analytic.account' , string='Assigned Analytic Account ' ,
                                                      domain="[('plan_id', '=', review_manager_id.plan_id.id)]" ,
-                                                     compute='_compute_analytic_account_id' , readonly=False , store=True )
+                                                     compute='_compute_analytic_account_id_assigned' , readonly=False , store=True )
     approve_uid = fields.Many2one ( 'res.users' , string='Approve User' , )
     approve_date = fields.Datetime ( string='Approve Date' )
     reject_reason = fields.Text ( string='Reject Reason' )
@@ -1045,54 +1045,67 @@ class SaleOrder ( models.Model ) :
     # rec.analytic_account_id = False
     # else :
     # rec.analytic_account_id = False
-
-    @api.depends ( 'project_type_id' , 'order_line' , 'review_manager_id' )
-    @api.onchange ( 'project_type_id' , 'order_line' , 'review_manager_id' )
-    def _compute_analytic_account_id(self) :
+    def _compute_analytic_account_id_assigned(self):
         for rec in self :
+            if rec.review_manager_id and rec.review_manager_id.plan_id :
+               name_base = account.analytic_account_id.name
 
-            rec.analytic_account_id = False
-            rec.analytic_account_id_assigned = False
+               assigned_account = self.env['account.analytic.account'].search ( [('name' , 'ilike' , name_base) ,
+               ('plan_id' , '=' , rec.review_manager_id.plan_id) ,] , limit=1 )
 
-            if not rec.order_line :
-                continue
+               rec.analytic_account_id_assigned = assigned_account.id
+            else:
+                rec.analytic_account_id_assigned = False
 
-            # =========================
-            # 1. الأساسي (حسب project_type)
-            # =========================
-            account = rec.order_line[0].product_id.product_analytic_ids.filtered (
-                lambda x : x.analytic_plan_id.id == rec.project_type_id.id
-            )
 
-            if account :
-                rec.analytic_account_id = account.analytic_account_id.id
 
-                # =========================
-                # 2. assigned (نفس الاسم داخل plan مختلف)
-                # =========================
-                if rec.review_manager_id and rec.review_manager_id.plan_id :
-                    name_base = account.analytic_account_id.name
-
-                    assigned_account = self.env['account.analytic.account'].search ( [
-                        ('name' , 'ilike' , name_base) ,
-                        ('plan_id' , '=' , rec.review_manager_id.plan_id.id) ,
-                    ] , limit=1 )
-
-                    rec.analytic_account_id_assigned = assigned_account.id
-
-    # @api.depends ( 'project_type_id' , 'order_line' )
-    # @api.onchange ( 'project_type_id' , 'order_line' ,'review_manager_id' )
+    # @api.depends ( 'project_type_id' , 'order_line' , 'review_manager_id' )
+    # @api.onchange ( 'project_type_id' , 'order_line' , 'review_manager_id' )
     # def _compute_analytic_account_id(self) :
     #     for rec in self :
-    #         if rec.order_line :
-    #             account = rec.order_line[0].product_id.product_analytic_ids.filtered (
-    #                 lambda x : x.analytic_plan_id.id == rec.project_type_id.id )
-    #             if account :
-    #                 rec.analytic_account_id = account.analytic_account_id.id
-    #             # else :
-    #             # rec.analytic_account_id = False
-    #         else :
-    #             rec.analytic_account_id = False
+
+    #         rec.analytic_account_id = False
+    #         rec.analytic_account_id_assigned = False
+
+    #         if not rec.order_line :
+    #             continue
+
+    #         # =========================
+    #         # 1. الأساسي (حسب project_type)
+    #         # =========================
+    #         account = rec.order_line[0].product_id.product_analytic_ids.filtered (
+    #             lambda x : x.analytic_plan_id.id == rec.project_type_id.id
+    #         )
+
+    #         if account :
+    #             rec.analytic_account_id = account.analytic_account_id.id
+
+    #             # =========================
+    #             # 2. assigned (نفس الاسم داخل plan مختلف)
+    #             # =========================
+    #             if rec.review_manager_id and rec.review_manager_id.plan_id :
+    #                 name_base = account.analytic_account_id.name
+
+    #                 assigned_account = self.env['account.analytic.account'].search ( [
+    #                     ('name' , 'ilike' , name_base) ,
+    #                     ('plan_id' , '=' , rec.review_manager_id.plan_id.id) ,
+    #                 ] , limit=1 )
+
+    #                 rec.analytic_account_id_assigned = assigned_account.id
+
+    @api.depends ( 'project_type_id' , 'order_line')
+    @api.onchange ( 'project_type_id' , 'order_line')
+    def _compute_analytic_account_id(self) :
+        for rec in self :
+            if rec.order_line :
+                account = rec.order_line[0].product_id.product_analytic_ids.filtered (
+                    lambda x : x.analytic_plan_id.id == rec.project_type_id.id )
+                if account :
+                    rec.analytic_account_id = account.analytic_account_id.id
+                # else :
+                # rec.analytic_account_id = False
+            else :
+                rec.analytic_account_id = False
 
     def action_open_payment(self) :
         return {
@@ -1196,7 +1209,7 @@ class SaleOrder2 ( models.Model ) :
     project_type_id = fields.Many2one ( 'account.analytic.plan' , string='Company Type' )
     # analytic_account_id = fields.Many2one ( 'account.analytic.account' , string='Analytic Account' ,
     #                                        domain="[('plan_id', '=', project_type_id)]" ,
-    #                                        compute='_compute_analytic_account_id' , readonly=False , store=True )
+    #                                        compute='_compute_analytic_account_id_assigned' , readonly=False , store=True )
     approve_uid = fields.Many2one ( 'res.users' , string='Approve User' , )
     approve_date = fields.Datetime ( string='Approve Date' )
     reject_reason = fields.Text ( string='Reject Reason' )
