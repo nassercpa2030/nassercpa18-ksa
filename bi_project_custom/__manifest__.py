6e5cb8380c344791488557e50f913ef53181c9b5
NK_IT <m.mohamed@nassercpa.com>
2026-07-14 13:29:26 +0300

    Update __manifest__.py

--- bi_project_custom/__manifest__.py
+++ bi_project_custom/__manifest__.py
@@ -20,7 +20,8 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','project','sale_project','sale','hr_timesheet','kbi_custom'],
+    # 'depends': ['base','project','sale_project','sale','hr_timesheet','kbi_custom'],
+    'depends': ['base','project','sale_project','sale','hr_timesheet','kbi_custom','hr','hr_attendance',],
 
     # always loaded
     'data': [
